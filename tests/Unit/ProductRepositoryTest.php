b76b0f83103d40a810ed527d2b389920e33c5454
Bárbara Luz <barbgluz@gmail.com>
2024-04-22 16:03:14 -0300

    test: it must get products by price

--- tests/Unit/ProductRepositoryTest.php
+++ tests/Unit/ProductRepositoryTest.php
@@ -49,6 +49,20 @@ final class ProductRepositoryTest extends TestCase
         }
     }
 
+    public function testGetByPrice(): void
+    {
+        $category = null;
+        $price = 20000;
+
+        $repository = new ProductRepository();
+
+        $products = $repository->getFiltered($price, $category);
+
+        $this->assertCount(1, $products);
+        $this->assertInstanceOf(Collection::class, $products);
+        $this->assertEquals(20000, $products[0]->price_original);
+    }
+
     protected function tearDown(): void
     {
         parent::tearDown();
