a5b1a35bc0d02d974eb38b1026d9aaaa233e46a0
Bárbara Luz <barbgluz@gmail.com>
2024-04-22 15:57:59 -0300

    test: it must get products from a category

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -43,6 +43,29 @@ final class ProductServiceTest extends TestCase
         $this->assertInstanceOf(Collection::class, $result);
     }
 
+    public function testItMustGetProductsFromACategory()
+    {
+        $category = 'insurance';
+        $price = null;
+        $jsonData = file_get_contents('tests/Unit/jsonData/insuranceCategoryProducts.json');
+        $data = new Collection(json_decode($jsonData, true));
+
+        $products = collect($data)->map(function ($productData) {
+            return new Product($productData);
+        });
+
+        $this->productRepository
+            ->shouldReceive('getFiltered')
+            ->with($price, $category)
+            ->once()
+            ->andReturn(new Collection($products));
+
+        $result = $this->productService->get($price, $category);
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertCount(2, $result);
+    }
+
     protected function tearDown(): void
     {
         parent::tearDown();
