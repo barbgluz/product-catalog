73289792484b7d79fc9c0b49fa20591d6e2a786f
Bárbara Luz <barbgluz@gmail.com>
2024-04-22 16:35:30 -0300

    test: tests the products endpoint with category param and returns a successful response

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -17,4 +17,17 @@ class ProductTest extends TestCase
         $response->assertStatus(200);
         $response->assertJson($data->toArray());
     }
+
+    public function test_the_products_endpoint_with_category_and_returns_a_successful_response(): void
+    {
+        $jsonData = file_get_contents('tests/Unit/jsonData/insuranceCategoryProducts.json');
+        $data = new Collection(json_decode($jsonData, true));
+        $category = 'insurance';
+
+        $response = $this->get('/products?category='.$category);
+
+        $response->assertStatus(200);
+        $response->assertJson($data->toArray());
+    }
+
 }
