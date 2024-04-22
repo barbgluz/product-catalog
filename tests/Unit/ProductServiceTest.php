5b20a155c57b1343d0e803ea10c91577038fec77
Bárbara Luz <barbgluz@gmail.com>
2024-04-22 15:58:38 -0300

    test: it must get products from price

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -66,6 +66,32 @@ final class ProductServiceTest extends TestCase
         $this->assertCount(2, $result);
     }
 
+    public function testItMustGetProductsFromPrice()
+    {
+        $category = null;
+        $price = 20000;
+        $jsonData = file_get_contents('tests/Unit/jsonData/insuranceCategoryAndPrice20000Products.json');
+        $data = new Collection(json_decode($jsonData, true));
+
+        $products = collect($data)->map(function ($productData) {
+            return new Product($productData);
+        });
+
+        $this->productRepository
+            ->shouldReceive('getFiltered')
+            ->with($price, $category)
+            ->once()
+            ->andReturn(new Collection($products));
+
+        $result = $this->productService->get($price, $category);
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertCount(1, $result);
+        foreach ($products as $product) {
+            $this->assertEquals($price, $product->price_original);
+        }
+    }
+
     protected function tearDown(): void
     {
         parent::tearDown();
