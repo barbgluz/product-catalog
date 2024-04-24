74571fbdfef4a08c2e313c7fcdbde19c1810372c
Bárbara Luz <barbgluz@gmail.com>
2024-04-24 11:20:21 -0300

    feat: implements caching

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -5,42 +5,53 @@ namespace App\Repositories;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class ProductRepository
 {
+    private int $minutes = 2;
+
     public function get(): Collection
     {
-        return Product::with(['price' => function ($query) {
-            $query->select('id', 'original', 'final', 'discount_percentage', 'currency', 'product_id');
-        }])->select([
-            'id',
-            'sku',
-            'name',
-            'category'
-        ])->get();
+        $cacheKey = 'products_all';
+
+        return Cache::remember($cacheKey, $this->minutes, function () {
+            return Product::with(['price' => function ($query) {
+                $query->select('id', 'original', 'final', 'discount_percentage', 'currency', 'product_id');
+            }])->select([
+                'id',
+                'sku',
+                'name',
+                'category'
+            ])->get();
+        });
     }
 
     public function getFiltered(?int $price, ?string $category): Collection
     {
-        $query = Product::query();
+        $cacheKey = 'products_filtered_' . $price . '_' . $category;
 
-        $query->when($price, function ($query, $price) {
-            $query->whereHas('price', function ($query) use ($price) {
-                $query->where('original', $price);
+        return Cache::remember($cacheKey, $this->minutes, function () use ($category, $price) {
+            $query = Product::query();
+
+            $query->when($price, function ($query, $price) {
+                $query->whereHas('price', function ($query) use ($price) {
+                    $query->where('original', $price);
+                });
             });
-        });
 
-        $query->when($category, function (Builder $query, $category) {
-            $query->where('category', $category);
-        });
+            $query->when($category, function (Builder $query, $category) {
+                $query->where('category', $category);
+            });
 
-        return $query->with(['price' => function ($query) {
-            $query->select('id', 'original', 'final', 'discount_percentage', 'currency', 'product_id');
-        }])->select([
-            'id',
-            'sku',
-            'name',
-            'category'
-        ])->get();
+            return $query->with(['price' => function ($query) {
+                $query->select('id', 'original', 'final', 'discount_percentage', 'currency', 'product_id');
+            }])->select([
+                'id',
+                'sku',
+                'name',
+                'category'
+            ])->get();
+        });
     }
 }
